77e0379be26a70511304d56dd6ea572e12322103
Victor Ciobanu <maxpointgames2@gmail.com>
2022-05-13 23:46:51 +0100

    main

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     module:{
         rules:[
             {
-                test: /\.s?css$/,
+                test: /\.(s[ac]|c)ss$/,
                 use:[MiniCssExtractPlugin.loader,"css-loader", "postcss-loader","sass-loader"] //allows CSS ,SCSS and postCSS support
             },
             
