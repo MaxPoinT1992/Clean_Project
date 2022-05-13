e5f40bcc8f0323b6be0a7fdede5f3d8aa65f07ae
Victor Ciobanu <maxpointgames2@gmail.com>
2022-05-13 23:44:59 +0100

    main

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 let mode = "development"
 
 if (process.env.NODE_ENV === "production"){
@@ -11,17 +13,26 @@ module.exports = {
     mode:mode,
 
     module:{
-        rules:[{
-            test:/\.js$/, //allow .js be used by babel
-            exclude: /node_modules/, //prevents babel from checking this folder
+        rules:[
+            {
+                test: /\.s?css$/,
+                use:[MiniCssExtractPlugin.loader,"css-loader", "postcss-loader","sass-loader"] //allows CSS ,SCSS and postCSS support
+            },
+            
+            {
+            test:/\.js$/, //allow .js be used by babel.
+            exclude: /node_modules/, //prevents babel from checking this folder.
             use:{
                 loader: "babel-loader" 
             }
         }]
     },
 
+    plugins: [new MiniCssExtractPlugin()],
+
     devtool:"source-map",
     devServer: {
-        static: "./dist"
+        static: "./dist",
+        hot:true    //shows changes on the project without refreshing it.
     }
 }
